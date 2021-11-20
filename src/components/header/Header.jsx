5faea20a40a9865f973d19423e7933abd9e91280
coder-mezab <k.rehmatulla22@gmail.com>
2021-11-20 21:07:29 +0530

    fixed minor issues

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import "./_header.scss";
 import { FaBars } from "react-icons/fa";
 import { AiOutlineSearch } from "react-icons/ai";
 import { MdNotifications, MdApps } from "react-icons/md";
+import "./_header.scss";
 
 const Header = () => {
   return (
