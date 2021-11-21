d3d341912ba3deb5b667af6aab18f2575d6d3a34
coder-mezab <k.rehmatulla22@gmail.com>
2021-11-21 17:10:30 +0530

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBfpGuccKcb9im4NiD8747GlHFGMoG7ypk",
+  authDomain: "meztube-com.firebaseapp.com",
+  projectId: "meztube-com",
+  storageBucket: "meztube-com.appspot.com",
+  messagingSenderId: "472231034408",
+  appId: "1:472231034408:web:09ccca24be9bf5f4767f1d",
+  measurementId: "G-8LQE8VMHJG",
+};
+
+firebase.initializeApp(firebaseConfig);
